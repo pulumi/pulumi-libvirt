9dc63223eb97e0c950aa536729e47cd623ac0257
Lee Briggs <lee@leebriggs.co.uk>
2021-04-09 12:03:36 -0700

    rollback

--- testing/setup.sh
+++ testing/setup.sh
@@ -4,7 +4,5 @@ set -x -ef -o pipefail
 sudo apt-get update
 sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system
 
-sudo virsh -c lxc:// define testing/template.xml
-sudo virsh -c lxc:// start testing
 sudo virsh -c lxc:// list
 
