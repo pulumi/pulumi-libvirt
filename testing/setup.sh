c4d5ac6c6febbabdb08e8061e0e0ce8e0045a4b7
Lee Briggs <lee@leebriggs.co.uk>
2021-04-09 12:14:54 -0700

    i hate args

--- testing/setup.sh
+++ testing/setup.sh
@@ -4,7 +4,7 @@ set -x -ef -o pipefail
 sudo apt-get update
 sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system
 
-sudo DOWNLOAD_KEYSERVER="pgp.mit.edu" lxc-create -t download -n foo --dist ubuntu --release bionic --arch amd64
+sudo DOWNLOAD_KEYSERVER="pgp.mit.edu" lxc-create -t download -n foo -- --dist ubuntu --release bionic --arch amd64
 
 sudo virsh -c lxc:// list
 
