c0e0f8639a2195143fb233ba11d7be27e0b51af8
Lee Briggs <lee@leebriggs.co.uk>
2021-04-09 11:57:41 -0700

    no really, make me a sandwich

--- testing/setup.sh
+++ testing/setup.sh
@@ -4,7 +4,7 @@ set -x -ef -o pipefail
 sudo apt-get update
 sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system
 
-virsh -c lxc:// define testing/template.xml
-virsh -c lxc:// start testing
-virsh -c lxc:// list
+sudo virsh -c lxc:// define testing/template.xml
+sudo virsh -c lxc:// start testing
+sudo virsh -c lxc:// list
 
