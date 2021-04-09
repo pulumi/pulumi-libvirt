2e59862fdad4b06e3d0b5dd171e283388b64461f
Lee Briggs <lee@leebriggs.co.uk>
2021-04-09 12:05:18 -0700

    create an lxc container

--- testing/setup.sh
+++ testing/setup.sh
@@ -4,5 +4,7 @@ set -x -ef -o pipefail
 sudo apt-get update
 sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system
 
+sudo lxc-create -t ubuntu -n foo
+
 sudo virsh -c lxc:// list
 
