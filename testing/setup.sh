9697117ea8d9174849e78394c15540685ae371ca
stack72 <public@paulstack.co.uk>
2021-04-12 11:23:49 +0100

    Running lxc without sudo

--- testing/setup.sh
+++ testing/setup.sh
@@ -4,6 +4,35 @@ set -x -ef -o pipefail
 sudo apt-get update
 sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system pkg-config
 
-sudo lxc-create -t download -n foo -- --dist ubuntu --release bionic --arch amd64 --no-validate
+id runner
+groups
 
-sudo virsh -c lxc:// list
+sudo usermod -aG libvirt $(whoami)
+
+id runner
+groups
+
+sudo newgrp libvirt
+groups
+
+sudo bash -c 'cat << EOF > /etc/libvirt/libvirtd.conf
+unix_sock_group = "libvirt"
+unix_sock_ro_perms = "0777"
+unix_sock_rw_perms = "0777"
+unix_sock_admin_perms = "0777"
+auth_unix_ro = "none"
+auth_unix_rw = "none"
+EOF'
+
+sudo systemctl restart libvirtd.service
+
+mkdir -p ~/.config/lxc
+cat << EOF > ~/.config/lxc/default.conf
+lxc.include = /etc/lxc/default.conf
+lxc.idmap = u 0 165536 65536
+lxc.idmap = g 0 165536 65536
+EOF
+
+lxc-create -t download -n foo -- --dist ubuntu --release bionic --arch amd64 --no-validate
+
+virsh -c lxc:// list
