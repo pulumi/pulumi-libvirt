1693381786b5c33d2c19cdd409c2a8e6afbac7c5
Lee Briggs <lee@leebriggs.co.uk>
2021-04-12 14:30:16 -0700

    debug

--- testing/setup.sh
+++ testing/setup.sh
@@ -4,6 +4,22 @@ set -x -ef -o pipefail
 sudo apt-get update
 sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system
 
+id
+
+groups $(id)
+
+sudo usermod -a -G libvirt $(whoami)
+sudo systemctl restart libvirtd.service
+
+cat /etc/libvirt/libvirtd.conf
+
+mkdir -p ~/.config/lxc
+cat << EOF > ~/.config/lxc/default.conf
+lxc.include = /etc/lxc/default.conf
+lxc.idmap = u 0 165536 65536
+lxc.idmap = g 0 165536 65536
+EOF
+
 sudo lxc-create -t download -n foo -- --dist ubuntu --release bionic --arch amd64 --no-validate
 
 sudo virsh -c lxc:// list
