bbc198e5720e6c80db7e8f62c4906118a526cd7e
Lee Briggs <lee@leebriggs.co.uk>
2021-04-12 18:13:57 -0700

    override perms on libvirt

--- testing/setup.sh
+++ testing/setup.sh
@@ -2,29 +2,20 @@
 set -x -ef -o pipefail
 
 sudo apt-get update
-sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system pkg-config
 
-id runner
-groups
+sudo mkdir -p /etc/systemd/system/libvirtd.socket.d/
+cat << EOF > /tmp/override.conf
+[Socket]
+SocketGroup=docker
+EOF
 
-sudo usermod -aG libvirt $(whoami)
+sudo mv /tmp/override.conf /etc/systemd/system/libvirtd.socket.d/
 
-id runner
-groups
-
-sudo newgrp libvirt
-groups
-
-sudo bash -c 'cat << EOF > /etc/libvirt/libvirtd.conf
-unix_sock_group = "libvirt"
-unix_sock_ro_perms = "0777"
-unix_sock_rw_perms = "0777"
-unix_sock_admin_perms = "0777"
-auth_unix_ro = "none"
-auth_unix_rw = "none"
-EOF'
+sudo apt-get install -y lxc libvirt-daemon libvirt-dev libvirt-daemon-driver-lxc libvirt-daemon-system
 
 sudo systemctl restart libvirtd.service
+
+ls -alh /var/run/libvirt/
 
 mkdir -p ~/.config/lxc
 cat << EOF > ~/.config/lxc/default.conf
