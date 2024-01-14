0ceb84bc7d8938b70af4a101cd071fa415ab813c
Ian Wahbe <ian@wahbe.com>
2024-01-13 18:44:34 -0800

    lint_provider.fix

--- provider/cmd/pulumi-resource-libvirt/main.go
+++ provider/cmd/pulumi-resource-libvirt/main.go
@@ -19,9 +19,10 @@ package main
 import (
 	_ "embed"
 
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+
 	libvirt "github.com/pulumi/pulumi-libvirt/provider"
 	"github.com/pulumi/pulumi-libvirt/provider/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
 
 //go:embed schema-embed.json
